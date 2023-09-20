f14ef432f6102be3b503fdf8ce23c2dc3cc73da1
ibnaclevaro <145083043+IbnaClevaro10@users.noreply.github.com>
2023-09-20 15:07:47 +0700

    Create biodata.kt

---
+++ biodata.kt
@@ -0,0 +1,9 @@
+fun main() {
+  var nama = "Ibna Clevaro Syarif"
+  var kelas = "XI RPL 2"
+  var hobi = "Basket"
+
+  println("Nama : $nama")
+  println("Kelas : $kelas")
+  println("Hobi : $hobi)
+}
